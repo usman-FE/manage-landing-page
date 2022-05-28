acb5f06652c23db8e4c7037f2b02057334b56cad
Usman-FE <ch.usman564@gmail.com>
2022-05-28 12:56:33 +0500

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./src/**/*.html', './src/app/**/*.js'],
+  theme: {
+    screens: {
+      'sm': '500px',
+      'md': '768px',
+      'lg': '1000px',
+      'xl': '1110px',
+    },
+    extend: {
+      colors: {
+        // PRIMARY COLORS
+        BrightRed: 'hsl(12, 88%, 59%)',
+        DarkBlue: 'hsl(228, 39%, 23%)',
+
+        // NEUTRALS
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)',
+        VeryDarkBlue: 'hsl(233, 12%, 13%)',
+        VeryPaleRed: 'hsl(13, 100%, 96%)',
+        VaryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+};
